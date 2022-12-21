84a4ee3cb384b9d74deffd0bc72823ccfd6c22fa
sol2kwon <amorfatisole@gmail.com>
2022-12-21 20:15:27 +0900

    20221221

--- src/main/java/solyitest/solyitest/ComparatorTest.java
+++ src/main/java/solyitest/solyitest/ComparatorTest.java
@@ -11,10 +11,12 @@ public class ComparatorTest {
         users.add(new User(3,"solyi"));
         users.add(new User(1,"moyi"));
         users.add(new User(2,"boyi"));
-        users.add(new User(5,"tttttttttttttest"));
 
         // u1<u2 음수리턴  , u1=u2 0리턴, u1>u2 양수리턴  -> int타입 리턴
         Comparator<User> idComparator =( ul,u2)-> ul.getId() - u2.getId();
         Collections.sort(users,idComparator);
+
+        Collections.sort(users,(u1,u2)->u1.getName().compareTo(u2.getName()));
+        System.out.println(users);
     }
 }
